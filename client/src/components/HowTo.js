eda265d7cfdf85fe03b5dd05c9a0934f0011a0d4
GauravArora <arora.gaurav0497@gmail.com>
2022-11-10 13:48:15 +0530

    changes in howTo page

--- client/src/components/HowTo.js
+++ client/src/components/HowTo.js
@@ -9,26 +9,26 @@ class HowTo extends Component {
                 <p>
                     To create a new survey, follow below steps:
                     <ol className='collection'>
-                        <li>
+                        <li className='collection-item'>
                             First, login to the application using your Google Id.
                         </li>
-                        <li>
+                        <li className='collection-item'>
                             Then, go to the dashboard page by clicking on the button at the top banner.
                         </li>
-                        <li>
+                        <li className='collection-item'>
                             Once you're on the Dashboard page, you'll be able to see your existing surveys with their details.
                             If you do not have any existing surveys, you can now create a new one by clicking on the +(plus) icon from the bottom right corner of the dashboard screen.
                         </li>
-                        <li>
+                        <li className='collection-item'>
                             After clicking on the +(plus) icon, fill in the details. Make sure to fill all the fields.
                         </li>
-                        <li>
+                        <li className='collection-item'>
                             To add multiple recipients, comma seperate the email Ids in the recipients field. Like abc@xyz.com, def@xyz.com,... etc.
                         </li>
-                        <li>
+                        <li className='collection-item'>
                             Then, click on next and you will able to verify the details you've entered before sending the actual survey out to your list of recipients.
                         </li>
-                        <li>
+                        <li className='collection-item'>
                             Confirm the details and click send. Single credit will be deducted from your account for every survey you have sent out(irrespective of number of recipients you have mentioned).
                         </li>
                     </ol>
